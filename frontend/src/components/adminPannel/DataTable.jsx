f916f3116968869138f761c918c1c67dc7407aa0
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-04-05 13:14:29 +0200

    finish first step Grid

--- frontend/src/components/adminPannel/DataTable.jsx
+++ frontend/src/components/adminPannel/DataTable.jsx
@@ -1,11 +1,28 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { DataGrid } from "@mui/x-data-grid/node";
 
 export default function DataTable() {
+  const [data, setData] = useState([]);
+
+  const getUserData = async () => {
+    await axios.get("http://localhost:5000/users").then((res) => {
+      setData(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  // eslint-disable-next-line no-restricted-syntax
+  // console.log(data);
+
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
+    { field: "name", headerName: "First name", width: 130 },
+    { field: "email", headerName: "email", width: 130 },
     {
       field: "age",
       headerName: "Age",
@@ -19,29 +36,23 @@ export default function DataTable() {
       sortable: false,
       width: 160,
       valueGetter: (params) =>
-        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+        `${params.row.name || ""} ${params.row.email || ""}`,
     },
   ];
 
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  ];
+  const rows = data.map((row) => ({
+    id: row.id,
+    name: row.name,
+    email: row.email,
+  }));
 
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: 400, width: "100%", margin: 150 }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={1}
+        rowsPerPageOptions={1}
         checkboxSelection
       />
     </div>
