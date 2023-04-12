0d80d998df1abe355cdbc927309a3dce0ada7c75
MaxGuet <maxence.guet@gmail.com>
2023-04-12 17:05:54 +0200

    CSS modified in SASS / ROUTES fixed

--- frontend/src/components/User/ConnectionPage.jsx
+++ frontend/src/components/User/ConnectionPage.jsx
@@ -1,4 +1,4 @@
-import "../../styles/ConnectionPage.css";
+import "../../styles/index.css";
 import { useState } from "react";
 
 export default function ConnectionPage() {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <p>Début du projet</p>
-    </div>
-  );
+  return <div />;
 }
