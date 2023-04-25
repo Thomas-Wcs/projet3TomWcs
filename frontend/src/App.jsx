8229b5ecfb059688599c6e7e396477f352e2c081
Jozipro <boschi.jonathan@gmail.com>
2023-04-25 12:28:19 +0200

    nettoyage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,35 +4,17 @@ import AdminPanel from "./components/adminPannel/AdminPanel";
 import Header from "./components/Header/Header";
 import ConnectionPage from "./components/User/ConnectionPage";
 import Profile from "./components/User/Profile";
-<<<<<<< HEAD
 import Home from "./pages/Home";
 import Advert from "./components/advertising/Advert";
 import Footer from "./components/footer/Footer";
-=======
 import DataTable from "./components/adminPannel/DataTable";
 import VideosManagement from "./components/adminPannel/VideosManagement";
 import Homepage2 from "./pages/Homepage2";
 import userContext from "./context/userContext";
->>>>>>> 25c939bf622826c705e446842a064849736a0e14
 
 function App() {
   const [userAuth, setUserAuth] = useState("");
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/connexion" element={<ConnectionPage />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/adminPanel/*" element={<AdminPanel />} />
-          <Route path="/advert" element={<Advert />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-=======
     <userContext.Provider value={(userAuth, setUserAuth)}>
       <div className="App">
         <Router>
@@ -45,11 +27,11 @@ function App() {
               <Route path="usersTable" element={<DataTable />} />
               <Route path="videosTable" element={<VideosManagement />} />
             </Route>
+            <Route path="/advert" element={<Advert />} />
           </Routes>
         </Router>
       </div>
     </userContext.Provider>
->>>>>>> 25c939bf622826c705e446842a064849736a0e14
   );
 }
 
