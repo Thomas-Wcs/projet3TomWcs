95d13ec788a58a9e34369748f7d52fcfcbd78488
MaxGuet <maxence.guet@gmail.com>
2023-04-26 16:17:34 +0200

    update video done

--- frontend/src/components/adminPannel/Video.jsx
+++ frontend/src/components/adminPannel/Video.jsx
@@ -1,19 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import useAPI from "../../api/useAPI";
 
 function Videos({ videos, onDeleteVideo }) {
+  const api = useAPI();
+
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const [newCategorie, setNewCategorie] = useState();
+  const [modify, setModify] = useState(false);
+
   const handleDeleteVideo = () => {
     onDeleteVideo(videos.id);
   };
 
+  const handleEdit = () => {
+    const newVideo = {
+      title: newTitle,
+      description_text: newDescription,
+      category_id: setNewCategorie,
+    };
+
+    api.put(`/videos/${videos.id}`, newVideo).then((resp) => {
+      console.log(resp);
+    });
+  };
+
+  const handleModify = () => {
+    setModify(!modify);
+  };
+
   return (
     <div className="admin-videos" style={{ backgroundColor: "grey" }}>
-      <h2>{videos.title}</h2>
-      <div>{videos.description_text} </div>
-      <p>Date d'ajout: {videos.date_publication}</p>
-      <button type="button" onClick={handleDeleteVideo}>
-        Supprimer
-      </button>
+      <div className="video-info">
+        <h2>{videos.title}</h2>
+        <div>{videos.description_text} </div>
+        <p>Date d'ajout: {videos.date_publication}</p>
+        <button type="button" onClick={handleDeleteVideo}>
+          Supprimer
+        </button>
+        <button type="button" onClick={handleModify}>
+          Modifier
+        </button>
+      </div>
+      {modify && (
+        <div className="video-update-form">
+          <label htmlFor="title">
+            Nouveau titre:
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+            />
+          </label>
+          <label htmlFor="description">
+            Nouvelle description:
+            <input
+              type="text"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+            />
+          </label>
+          <select onChange={(e) => setNewCategorie(e.target.value)}>
+            <option value="1">Animaux</option>
+            <option value="2">Sports</option>
+            <option value="3">Cuisine</option>
+            <option value="4">Voyage</option>
+          </select>
+          <button type="button" onClick={handleEdit}>
+            {" "}
+            Modifier
+          </button>
+        </div>
+      )}
     </div>
   );
 }
