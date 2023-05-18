149378c9d954550e2b395c902cbf17a542cedc21
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-05-18 12:21:00 +0200

    add get in userfavortie

--- frontend/src/components/dashbord/UserFavorite.jsx
+++ frontend/src/components/dashbord/UserFavorite.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAPI from "../../api/useAPI";
 
 export default function UserFavorite() {
+  const [data, setData] = useState([]);
+  // eslint-disable-next-line no-restricted-syntax
+  console.log(data);
+  const api = useAPI();
+
+  const getVideoData = async () => {
+    await api
+      .get("videos")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  useEffect(() => {
+    getVideoData();
+  }, []);
+
   return (
     <div style={{ color: "white" }}>
       <p>VIDOES FAVORITES</p>
+      <div>
+        <p>name : </p>
+        <p>description: </p>
+        <p>lien :</p>
+      </div>
     </div>
   );
 }
