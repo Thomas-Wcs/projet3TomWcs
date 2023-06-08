012c325c3c18ef834cd0c96e3a6c3618bf842d2e
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-06-08 17:13:48 +0200

    about section 1.2

--- frontend/src/components/about/AboutPage.jsx
+++ frontend/src/components/about/AboutPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./aboutPage.css";
-import logoOrigins from "../../assets/images.png";
+import filmbanderole from "../../assets/filmband.png";
 import useAPI from "../../api/useAPI";
 
 function AboutPage() {
@@ -31,7 +31,44 @@ function AboutPage() {
 
   return (
     <div className="about-main-section">
-      <img src={logoOrigins} alt="" />
+      <img className="film-banderole-about" src={filmbanderole} alt="" />
+      <div className="texte-about-propos">
+        <h1>A propos</h1>
+        <p className="about-para-profil-section">
+          Le meilleur des films rien que pour vous
+        </p>
+        <p className="about-para-profil-section">
+          Ce site est un projet étudiant réalisé dans le cadre de la formation
+          Web Developpeur full-stack de la Wild Code School de Lyon.
+          <br />
+          <br /> Le projet professionnel de développement web réalisé par des
+          étudiants consistait à créer une plateforme OTT (Over-The-Top) de
+          streaming vidéo. Ce projet ambitieux visait à offrir aux utilisateurs
+          une expérience de divertissement en ligne exceptionnelle, en mettant à
+          leur disposition une vaste bibliothèque de contenus vidéo de haute
+          qualité. <br /> L'équipe d'étudiants a mis en place une architecture
+          solide pour le site de streaming, en utilisant les dernières
+          technologies web et en se conformant aux meilleures pratiques de
+          développement. <br /> Ils ont conçu une interface utilisateur
+          conviviale et attrayante, permettant aux utilisateurs de naviguer
+          facilement à travers les différentes sections de la plateforme. L'une
+          des principales fonctionnalités du site était la possibilité pour les
+          utilisateurs de créer des profils personnalisés. Ces profils leur
+          permettaient de sauvegarder leurs vidéos préférées. <br />
+          <br />
+          Ce projet a été une expérience enrichissante pour les étudiants, leur
+          permettant de mettre en pratique leurs connaissances et de se préparer
+          à une carrière prometteuse dans le domaine du développement web et du
+          divertissement numérique.
+          <br />
+          <br />
+          Merci à Guillaume Cregut et à la WCS de Lyon pour cette formation très
+          enrichissante !
+          <br />
+          <br />
+          Voici une brève présentation de l'équipe
+        </p>
+      </div>
       {data.slice(0, 4).map((item, index) => (
         <section
           className="hidden"
@@ -39,18 +76,16 @@ function AboutPage() {
           // eslint-disable-next-line no-return-assign
           ref={(ref) => (hiddenElementsRef.current[index] = ref)}
         >
-          <img src="" alt="developpeur" />
-          <h2> NOM : {item.name} </h2>
-          <p>
-            Bienvenue sur notre plateforme vidéo en ligne ! Nous nous engageons
-            à vous fournir une large sélection de vidéos de haute qualité pour
-            votre divertissement. Que vous recherchiez des films, des séries
-            télévisées, des documentaires ou du contenu éducatif, nous avons ce
-            qu'il vous faut.
-          </p>
-          <button type="button">Contactez-nous</button>
+          <img
+            className="profil-about-image"
+            src={`${import.meta.env.VITE_APP_API_URL}${item.avatar}`}
+            alt="developpeur"
+          />
+          <h2> {item.name} </h2>
+          <p className="about-para-profil-section">{item.descriptionProfil}</p>
         </section>
       ))}
+      <div>COUCOU</div>
     </div>
   );
 }
