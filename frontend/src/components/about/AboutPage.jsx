7bb5df9d43e5f4f91c3caaee301acf1f99a76e69
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-06-08 10:29:48 +0200

    rectify data map

--- frontend/src/components/about/AboutPage.jsx
+++ frontend/src/components/about/AboutPage.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./aboutPage.css";
 import logoOrigins from "../../assets/images.png";
+import useAPI from "../../api/useAPI";
 
 function AboutPage() {
+  const [data, setData] = useState([]);
+  const api = useAPI();
   const hiddenElementsRef = useRef([]);
+  const toto = useRef([]);
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
@@ -22,41 +26,30 @@ function AboutPage() {
     return () => {
       hiddenElementsRef.current.forEach((el) => observer.unobserve(el));
     };
+  }, [data]);
+
+  useEffect(() => {
+    api.get("users").then((res) => {
+      setData(res.data);
+    });
   }, []);
-
-  const hiddenElements = [];
-  // eslint-disable-next-line no-restricted-syntax
-  console.log(hiddenElements);
-  // eslint-disable-next-line no-plusplus
-  for (let index = 0; index < 4; index++) {
-    const ref = useRef(null);
-    hiddenElements.push(ref);
-
-    useEffect(() => {
-      if (ref.current) {
-        hiddenElementsRef.current.push(ref.current);
-      }
-    }, [ref]);
-  }
 
   return (
     <div className="about-main-section">
-      {hiddenElements.map((ref, index) => (
-        <section className="hidden" key={uuidv4()} ref={ref}>
-          <h2>Section {index + 2}</h2>
+      {data.map((item, index) => (
+        <section
+          className="hidden"
+          key={uuidv4()}
+          // eslint-disable-next-line no-return-assign
+          ref={(ref) => (hiddenElementsRef.current[index] = ref)}
+        >
+          <h2 ref={toto}> NOM : {item.name} </h2>
           <p>
             Bienvenue sur notre plateforme vidéo en ligne ! Nous nous engageons
             à vous fournir une large sélection de vidéos de haute qualité pour
             votre divertissement. Que vous recherchiez des films, des séries
             télévisées, des documentaires ou du contenu éducatif, nous avons ce
             qu'il vous faut.
-          </p>
-          <p>
-            Notre option premium offre un accès exclusif à encore plus de
-            contenus passionnants. Passez à la version premium et débloquez un
-            monde de vidéos supplémentaires, de streaming sans publicité et de
-            téléchargements hors ligne. Ne manquez pas cette expérience de
-            visionnage améliorée !
           </p>
           <img src={logoOrigins} alt="" />
           <button type="button">Contactez-nous</button>
