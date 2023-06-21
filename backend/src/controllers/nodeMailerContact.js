28ea4bd1273236c2e483a4bdbc5c1e00c5703da4
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-06-21 17:06:34 +0200

    finalisation de nodemailer inscription / contact

--- backend/src/controllers/nodeMailerContact.js
+++ backend/src/controllers/nodeMailerContact.js
@@ -50,7 +50,18 @@ function welcomeMessage(req, res) {
       to: email, // utilisé comme adresse RCPT TO: pour SMTP
     },
     subject: "Contact Client",
-    text: `Merci pour votre inscription ${name}, avec les identifiants suivant : ${email} "`,
+    html: `<p>Bonjour ${name}, <br> <br> Félicitations pour votre inscription et bienvenue sur le site Origins Digital avec l'adresse email suivante : ${email} ! <br>
+    Toute l'équipe est disponible pour répondre à vos questions. <br>
+    Si vous souhaitez changer votre mot de passe, veuillez cliquer sur le lien suivant :
+    <a href="http://localhost:5173/profile/useredit">Cliquez sur ce
+   lien</a><br><br>
+   Vous pouvez également le faire depuis votre profil utilisateur. Explorez toutes les vidéos à votre disposition et abonnez-vous pour profiter d'un contenu exceptionnel ! 
+
+    Cordialement, l'équipe de Origins Digital<br><br>
+   
+    <img style='display:block; width:244px;height:144px;' id='base64image'
+       src="https://img.newstank.fr/crop/none/f5fdf7057d8dd8cef45682b30206bb3e/0/0/672/294/640/280/image-article-221347.jpg" /> 
+    </p>`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
